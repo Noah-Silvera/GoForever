6a0484d66db9a0f4583a508a6a57c1ffa2f04ad1
Noah Silvera <thundoon@gmail.com>
2016-07-12 19:42:12 -0700

    Mock out model base class with fake returns for testing

--- src/scripts/frontend/models/model.js
+++ src/scripts/frontend/models/model.js
@@ -3,34 +3,59 @@ define(['requestHandler'],function(RequestHandler){
     return class Model{
         constructor(){
             this.data = null;
+            this.id = null;
+            this.model = null;
         }
 
-        retrieveData(){
-            RequestHandler.getData(this.modelName, this.id).then(function(data){
-                return setData(data)
-            },function(err){
-                console.error('could not retrieve model data')
-                throw err
-            })
+        ////////////////////////////////////////////////
+        // public methods
+        ////////////////////////////////////////////////
+
+        setData(gameData){
+            return new Promise((function(resolve,reject){
+                console.error('----- NOT IMPLEMENTENED ---- data adding')
+                console.error('temporary return value in place')
+                resolve(this.data) // this line is temporary
+
+                // send it to the DB and validate it first
+                RequestHandler.sendData(this.model,this.id, this.data).then((function(){
+                    // the game data is valid
+                    resolve(this.data)
+                }).bind(this))
+            }).bind(this))
+            // rejection occurs if inner promise fails 
         }
 
-        sendData(){
-            //
-        }
-
-        /**
-         * Getter
-         */
         getData(){
-            return this.data; 
+            return new Promise((function(resolve,reject){
+                console.error('----- NOT IMPLEMENTENED ---- data retrieval')
+                console.error('temporary return value in place')
+                resolve(this.data)
+
+                RequestHandler.sendData(this.model, this.id).then((function(returnedData){
+                    this.data = returnedData
+                    resolve(returnedData)
+                }).bind(this))
+            }).bind(this))
+
         }
 
-        /**
-         * Setter
-         */
-        setData(d){
-            this.data = d;
+        getProp(prop){
+            return new Promise((function(resolve,reject){
+                this.getData().then(function(){
+                    if( this.data.prop !== undefined || this.data.prop !== null ){
+                        resolve(this.data.prop)
+                    } else {
+                        reject('data property is undefined or null')
+                    }
+                })
+            }))
+
         }
+
+        ////////////////////////////////////////////////
+        // private methods
+        ///////////////////////////////////////////////
     }
     
 })
