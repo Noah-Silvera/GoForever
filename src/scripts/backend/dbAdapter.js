f5eb48b97575e20b536e83e7328b50b23f8d8632
Noah Silvera <thundoon@gmail.com>
2016-07-22 12:27:55 -0700

    jahsd

--- src/scripts/backend/dbAdapter.js
+++ src/scripts/backend/dbAdapter.js
@@ -19,7 +19,7 @@ class DBAdapter {
 
         // name in mongoDB
         this.dbName = 'GoForever'
-        this.host = 'localhost'
+        this.host = 'mongodb://localhost'
         this.port = '27017'
         this.url = `${this.host}:${this.port}/${this.dbName}`
 
